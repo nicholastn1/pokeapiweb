c61ac591e937fd8dbe1340d35eb8f31099abee4b
nicholastn1 <nicholastimbo2022@gmail.com>
2023-09-09 13:05:28 -0300

    Create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
 import './App.css';
 import { GET_POKEMON } from '../api';
+import SearchBar from './components/SearchBar/SearchBar';
+import LoadingIndicator from './components/LoadingIndicator/LoadingIndicator';
+import PokemonCard from './components/PokemonCard/PokemonCard';
 
 function App() {
-  const [pokemonSearchName, setPokemonSearchName] = useState('');
   const [pokemonAbilities, setPokemonAbilities] = useState([]);
   const [pokemonName, setPokemonName] = useState([]);
   const [pokemonSprite, setPokemonSprite] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  const fetchPokemonData = async () => {
+  const fetchPokemonData = async (pokemonSearchName) => {
     try {
       setLoading(true);
       setError(null);
@@ -38,64 +40,20 @@ function App() {
     }
   };
 
-  const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
-      fetchPokemonData();
-    }
-  };
-
   return (
     <div className="App">
-      <div className="search-container">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Enter Pokémon Name"
-          value={pokemonSearchName}
-          onChange={(e) => setPokemonSearchName(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-        <button className="search-button" onClick={fetchPokemonData}>
-          Search
-        </button>
-      </div>
-      {loading && <p className="loading-text">Loading...</p>}
+      <SearchBar onSearch={fetchPokemonData} />
+      {loading && <LoadingIndicator />}
       {!pokemonSprite && !pokemonAbilities.length && !error ? null : (
-        <div className="card">
-          {pokemonSprite && (
-            <div className="upper-container">
-              <div className="sprite-container">
-                <img
-                  className="sprite"
-                  src={pokemonSprite}
-                  alt={pokemonSearchName}
-                />
-              </div>
-              <h2>{pokemonName}</h2>
-            </div>
-          )}
-          {pokemonAbilities.length > 0 && (
-            <div className="abilities-container">
-              <ul>
-                {pokemonAbilities.map((ability, index) => (
-                  <li key={index}>{ability}</li>
-                ))}
-              </ul>
-            </div>
-          )}
-          {error ? (
-            <div className="error-container">
-              <img
-                className="error-image"
-                src="https://imgur.com/MGh60KR.png"
-                alt="No Results"
-              />
-              Invalid Pokémon!
-            </div>
-          ) : null}
-        </div>
+        <PokemonCard
+          pokemonName={pokemonName}
+          pokemonSprite={pokemonSprite}
+          pokemonAbilities={pokemonAbilities}
+          error={error}
+        />
       )}
     </div>
   );
 }
+
 export default App;
